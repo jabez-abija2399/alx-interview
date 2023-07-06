59c69303ad5f5acf249c0aa7d1463b5a0373f3da
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-07-06 02:09:27 -0700

    0-lockboxes.py is added and within it iswas added You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened. """
+    n = len(boxes)
+    myList = [0]
+    for i in myList:
+        for j in boxes[i]:
+            if j not in myList:
+                if j < n:
+                    myList.append(j)
+    if len(myList) == n:
+        return True
+    return False
